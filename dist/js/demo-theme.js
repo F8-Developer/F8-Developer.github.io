76f1cf27dc62e2fa3daceca6dc26e860f68fcc36
Ax7-cmd <>
2023-07-16 13:07:08 +0700

    update index html

--- dist/js/demo-theme.js
+++ dist/js/demo-theme.js
@@ -1,10 +1,19 @@
+/*!
+* Tabler v1.0.0-beta19 (https://tabler.io)
+* @version 1.0.0-beta19
+* @link https://tabler.io
+* Copyright 2018-2023 The Tabler Authors
+* Copyright 2018-2023 codecalm.net Paweł Kuna
+* Licensed under MIT (https://github.com/tabler/tabler/blob/master/LICENSE)
+*/
 (function (factory) {
 	typeof define === 'function' && define.amd ? define(factory) :
 	factory();
 })((function () { 'use strict';
+
 	var themeStorageKey = "tablerTheme";
 	var defaultTheme = "dark";
-	var selectedTheme = "dark";
+	var selectedTheme;
 	var params = new Proxy(new URLSearchParams(window.location.search), {
 	  get: function get(searchParams, prop) {
 	    return searchParams.get(prop);
